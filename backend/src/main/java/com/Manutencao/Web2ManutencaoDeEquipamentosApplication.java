a76beaad0d67bd0ac5764637c5d73913b066e17b
Matheus Dionysio <maditheon@gmail.com>
2025-08-24 20:11:18 -0300

    Dockerização completa do backend

--- backend/src/main/java/com/Manutencao/Web2ManutencaoDeEquipamentosApplication.java
+++ backend/src/main/java/com/Manutencao/Web2ManutencaoDeEquipamentosApplication.java
@@ -1,13 +1,13 @@
-package com.backend;
+package com.Manutencao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Web2ManutencaoDeEquipamentosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Web2ManutencaoDeEquipamentosApplication.class, args);
 	}
 
 }
--- backend/src/test/java/com/Manutencao/Web2ManutencaoDeEquipamentosApplicationTests.java
+++ backend/src/test/java/com/Manutencao/Web2ManutencaoDeEquipamentosApplicationTests.java
@@ -1,10 +1,10 @@
-package com.backend;
+package com.Manutencao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class Web2ManutencaoDeEquipamentosApplicationTests {
 
 	@Test
 	void contextLoads() {
